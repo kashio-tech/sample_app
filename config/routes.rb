b4769e5d69211f1f5a1b5129496f1cda78a01dd1
kashio-tech <gumgum.yk.29@gmail.com>
2019-11-07 18:58:06 +0900

    [finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	get 'top' => 'root#top'
   	post 'todolists' => 'todolists#create'
+
+  	get 'todolists' => 'todolists#index'
+  	get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  	#.../todolists/１や.../todolists/3に該当する
 end
