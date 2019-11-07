b5dd96b85cf23956d943cf01f2a5292fd9f3388e
kashio-tech <gumgum.yk.29@gmail.com>
2019-11-07 15:13:07 +0900

    [finish]3-4

--- db/migrate/20191106121541_create_lists.rb
+++ db/migrate/20191106121541_create_lists.rb
@@ -1,8 +1,10 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
-      t.timestamps
+    	#以下の２行を追加
+    	t.string :title
+    	t.string :body
+      	t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_121541) do
+ActiveRecord::Schema.define(version: 2019_11_07_060921) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
