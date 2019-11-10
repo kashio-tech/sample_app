076e7f0c00e6b0a5c395ac6d828625ef63d54962
kashio-tech <gumgum.yk.29@gmail.com>
2019-11-10 12:56:43 +0900

    [finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   	#.../todolists/１や.../todolists/3に該当する
   	get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   	patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  	delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
